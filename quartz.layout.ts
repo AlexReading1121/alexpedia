0f2b678750d5d75837a81934cbcd6d54f7ad89b3
Alexpedia <alexreading1121@gmail.com>
2026-07-05 16:07:55 +0800

    Remove ContentMeta, add back TableOfContents

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,7 +16,6 @@ export const defaultContentPageLayout: PageLayout = {
       component: Component.Breadcrumbs(),
       condition: (page) => page.fileData.slug !== "index",
     }),
-    Component.ContentMeta(),
   ],
   left: [
     Component.PageTitle(),
@@ -32,7 +31,9 @@ export const defaultContentPageLayout: PageLayout = {
       ],
     }),
   ],
-  right: [],
+  right: [
+    Component.DesktopOnly(Component.TableOfContents()),
+  ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
